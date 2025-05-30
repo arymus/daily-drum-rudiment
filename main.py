bbb9ffedb6e3d9e1a6bb8a6191d97367a4769ac1
Arymus Reyes <arymusr@gmail.com>
2025-05-30 15:43:13 -0400

    Styled UI, fixed bugs such as accidental root page scraping returning an error, and reworked time display

--- main.py
+++ main.py
@@ -10,29 +10,26 @@ def start_timer(seconds):
 
     # If timer_label is None (doesn't exist)
     if timer_label == None:
-        timer_label = tk.Label(win) # Create a tkinter label containing the amount of time left on the timer
-        timer_label.pack() # Add the timer to the window
+        timer_label = tk.Label(win, bg = "#EDD9CC", fg = "#B20808", font = ("Arial", 15, "bold")) # Create a tkinter label containing the amount of time left on the timer
+        timer_label.pack(ipadx = 10, ipady = 10, padx = 10, pady = 10) # Add the timer to the window
     # ^ If else, don't do anything
 
     # Function to call every second the timer runs
     def update_timer(remaining):
 
-        print(remaining)
-
         # If the remaining time is greater or equal to 0
         if remaining >= 0:
-            from math import ceil # Import ceil from the math module which rounds numbers up
 
             # If the remaining time is greater than or equal to 60, 
-            # set the display message to display the remaining amount of minutes by rounding up the amount of seconds divided by 60
-            if remaining >= 60: display = f"There are {ceil(remaining / 60)} minutes left."
+            # set the display message to display the remaining amount of minutes and seconds
+            if remaining >= 60: display = f"{remaining // 60}:{(remaining % 60):02d}"
             else: display = f"There are {remaining} seconds left." # If else (seconds are less than 60), display the amount of seconds
 
             timer_label.config(text = display) # Configure the timer label to contain the display message
             win.after(1000, update_timer, remaining - 1) # After 1000 millliseconds, the function calls itself and decreases remaining by 1
 
         # If else (the timer is 0), change the text of the label to "Time's up!"
-        else: timer_label.config(text="Time's up!")
+        else: timer_label.config(text = "Time's up!")
 
     update_timer(seconds) # Call the update_timer function with the amount of seconds passed to it
 
@@ -46,8 +43,7 @@ def open_image(filename):
         return None # Return none
 
     img = Image.open(filename) # Open the file as an image
-    img = img.resize((800, 500))
-    print("Image mode:", img.mode, "size:", img.size) # Print image data (mode: the colors it uses, size: the size in bytes)
+    img = img.resize((800, 500)) # Resize the image to be 800x500
     return ImageTk.PhotoImage(img) # Return the image as a TK PhotoImage
 
 # Function to delete image data with an event passed to it
@@ -67,18 +63,16 @@ def delete_image_data(event):
 
 win = tk.Tk() # Create a window
 win.geometry("1000x1000") # Make the window 1000 x 1000 px
-win.configure(bg = "purple") # Give it a purple background
+win.configure(bg = "#8b0909") # Give it a purple background
 win.title("Daily Drum Rudiments") # Give it a title of "Daily Drum Rudiments"
 
 # We store the result in the daily_rudiment variable to ensure it's only called once
 daily_rudiment = web_scraper.get_rudiment() # Call the get_rudiment function from web_scraper.py
+print(daily_rudiment["name"])
 
-rudiment_label = tk.Label(win, text = daily_rudiment["name"], fg = "white") # Create labels using the rudiment name retrieved from the dictionary get_rudiment returns
+rudiment_label = tk.Label(win, text = daily_rudiment["name"], font = ("Arial", 20, "bold"), fg = "#806350") # Create labels using the rudiment name retrieved from the dictionary get_rudiment returns and display them using the Impact font in bold, size 20px
 rudiment_label.bind("<Button-1>", lambda _: webbrowser.open_new(daily_rudiment["url"])) # Bind an event listener to the rudiment_url that opens the URL in the browser on a left click
-rudiment_label.pack() # Add the rudiment label to the window
-
-timer_button = tk.Button(win, text = "Start timer", command = lambda: start_timer(3600)) # Create a button that calls the timer function when clicked
-timer_button.pack() # Add the timer button to the window
+rudiment_label.pack(padx = 20, pady = 20, ipadx = 10, ipady = 10) # Add the rudiment label to the window with internal and external padding (10px and 20px respectively)
 
 video = web_scraper.get_video(daily_rudiment) # Call the get_video() function from web_scraper.py with the rudiment passed to it
 thumbnail_file = web_scraper.get_thumbnail(video) # Call the get_thumbnail() function from web_scraper.py with the video passed to it
@@ -87,6 +81,9 @@ thumbnail_img = open_image(thumbnail_file) # Store the thumbnail inside a variab
 video_label = tk.Label(win, image = thumbnail_img) # Create a label that displays the video
 video_label.bind("<Button-1>", lambda _: webbrowser.open_new(video)) # Bind an event listener to the label that opens the video in the browser on left click
 video_label.pack() # Add the label to the window
+
+timer_button = tk.Button(win, text = "Start timer", command = lambda: start_timer(1800), bg = "#806350", fg = "#EDD9CC", font = ("Arial", 15, "bold")) # Create a button that calls the timer function when clicked with colors and fonts applied
+timer_button.pack(padx = 10, pady = 10, ipadx = 10, ipady = 10) # Add the timer button to the window with internal and external padding
 
 win.bind("<Destroy>", delete_image_data) # Bind an event listener that invokes delete_image_data with _ as the event when the window is deleted
 win.mainloop() # Run the main loop so that the window appears
--- web_scraper.py
+++ web_scraper.py
@@ -1,12 +1,11 @@
-from bs4 import BeautifulSoup
-import requests; import random
+# Imports
+from bs4 import BeautifulSoup # BeautifulSoup for performing web scrapes
+import requests; import random # Requests for sending HTTP requests and random for generating random items from a list
 
 # Function to scrape data at a certain URL and parse it into HTML
 def scrape_soup(url):
     # HTTP headers to send along with the requests we make
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36' # User agent which contains data about my laptop to ensure the correect content is served in the response
-    }
+    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'} # User agent which contains data about my laptop to ensure the correect content is served in the response
 
     # Send a GET request to the URL
     # Send the headers from before as the request header
@@ -24,31 +23,32 @@ def get_rudiment():
 
     # links_html is a list that iterates through every <a> tag with an href attribute as a and filters out the ones that do not have a class attribute
     links_html = [a for a in content.find_all("a", href = True) if not a.has_attr("class")]
-    links = [] # An empty list for storing the raw links
+    rudiments = [] # An empty list for storing the rudiments
 
+    links_html.pop(0) # Remoe the first item from the list because the first item is a link to the home page, which causes an error if it's selected as the rudiment
     # For each link in links_html, where an individual link is i
     for i in links_html:
-        link = i.get("href") # Get the href attribute of i, the link
 
         # Create a rudiment_data dictionary to hold data
         rudiment_data = {
             "name": i.string, # Get the text content from the <a> tag and set it as the name of the rudiment
-            "url": f"https://www.40drumrudiments.com{link}" # Concatenate the link with the homepage as the URL because the link returns as a subdirectory of the homepage
+            "url": f"https://www.40drumrudiments.com{i.get('href')}" # Concatenate the href attribute with the homepage as the URL because the link returns as a subdirectory of the homepage
         }
 
-        links.append(rudiment_data) # Add the rudiment data to the list of raw links
-    
-    return random.choice(links) # Return a randomly rudiment from the list of raw links
+        rudiments.append(rudiment_data) # Add the rudiment data to the list of raw links
 
+    return random.choice(rudiments) # Return a randomly rudiment from the list
 
 # Function to get the youtube tutorial on how to play the rudiment
 def get_video(rudiment):
     content = scrape_soup(rudiment["url"]) # Get the HTML content from the rudiment's URL
     video_frame = content.find("iframe") # Return the <iframe> tag in the page, which holds the youtube tutorial video
-    
-    iframe_content = scrape_soup(video_frame.get("src")) # Get the HTML content from the link in the src attribute of the <iframe> tag
+
+    # Try to get the src attribute from the video frame but if there's an AttributeError print an error message
+    try: iframe_content = scrape_soup(video_frame.get("src")) # Get the HTML content from the link in the src attribute of the <iframe> tag
+    except AttributeError: print(rudiment["name"] + "returned None :(") # Print the rudiment's name in an error message on AttributeError
+
     link = iframe_content.find("a") # Retrieve the first <a> tag from the HTML
-    
     return link.get("href") # Return the href of the <a> tag (the website it takes you to)
 
 
@@ -99,7 +99,7 @@ def get_thumbnail(url):
             # If else (image scrape is empty)
             else:
                 print("Image content is empty!") # Print warning
-        
+
         # If else (status code isn't 200, aka an ERROR)
         else:
             print("HTTP error! Status code: " + str(img_scrape.status_code)) # Print HTTP error

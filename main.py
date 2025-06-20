6ee52e639d33658b80908801c982d3a5640b6e79
arymus <arymusr@gmail.com>
2025-06-20 19:38:41 +0100

    Added pause/play button

--- main.py
+++ main.py
@@ -2,20 +2,46 @@
 import tkinter as tk # Tkinter
 from PIL import Image, ImageTk # Import image modules from PIL (Python Imaging Library)
 import web_scraper; import webbrowser; # Import web_scraper.py and webbrowser module
+
 timer_label = None # Initialize timer_label as None
+pause_button = None # Initialize pause_button as None
+
+# Function to pause the timer
+def pause(button, time, timer, remaining):
+    win.after_cancel(timer) # Stop the timer
+
+    if remaining >= 60: time.config(text = f"{remaining // 60}:{(remaining % 60):02d}") # If the remaining time is a minute or above, display the time in minutes:seconds
+    else: time.config(text = f"There are {remaining} seconds left.") # If the remaining seconds are less than a minute, display only the seconds
+
+    button.config(text = "Play", command = lambda: start_timer(remaining)) # Change the text and function of the button
+
 
 # Function for starting a timer with a set amount of seconds
 def start_timer(seconds):
     global timer_label # Define timer_label as a global variable
+    global pause_button # Define pause_button as a global variable
 
     # If timer_label is None (doesn't exist)
     if timer_label == None:
         timer_label = tk.Label(win, bg = "#EDD9CC", fg = "#B20808", font = ("Arial", 15, "bold")) # Create a tkinter label containing the amount of time left on the timer
         timer_label.pack(ipadx = 10, ipady = 10, padx = 10, pady = 10) # Add the timer to the window
-    # ^ If else, don't do anything
+        timer_button.config(command = None) # Change the command of the timer button to do nothing so that it doesn't interfere with the main timer
+
+    # If the pause button is still None
+    if pause_button == None:
+        pause_button = tk.Button(win, text = "Pause", bg = "#B20808", fg = "#EDD9CC") # Create button
+        pause_button.pack(ipadx = 10, ipady = 10, padx = 10, pady = 10) # Add the button to the window
+
+    # If else, because when we call the function again on button click in pause(), the pause_button isn't None and therefore keeps the text of "Pause"
+    else:
+        pause_button.config(text = "Pause") # Edit the pause button to say pause
 
     # Function to call every second the timer runs
     def update_timer(remaining):
+
+        # The win.after() is assigned to the variable so that we can cancel it when the timer ends using it
+        timer = win.after(1000, update_timer, remaining - 1) # After 1000 millliseconds, the function calls itself and decreases remaining by 1
+        pause_button.config(command = lambda: pause(pause_button, timer_label, timer, remaining)) # Add the function to pause the timer onto the button as a command
 
         # If the remaining time is greater or equal to 0
         if remaining >= 0:
@@ -26,12 +52,29 @@ def start_timer(seconds):
             else: display = f"There are {remaining} seconds left." # If else (seconds are less than 60), display the amount of seconds
 
             timer_label.config(text = display) # Configure the timer label to contain the display message
-            win.after(1000, update_timer, remaining - 1) # After 1000 millliseconds, the function calls itself and decreases remaining by 1
 
         # If else (the timer is 0), change the text of the label to "Time's up!"
-        else: timer_label.config(text = "Time's up!")
+        else:
+            timer_label.config(text = "Time's up!") # Change the text of the timer label
+            timer_button.config(command = lambda: start_timer(30)) # Add the timer command back to the timer button
+
+            def erase_label(id):
+                global timer_label # Define timer_label as a global variable
+                global pause_button # Define pause_button as a global variable
+                
+                win.after_cancel(id) # Cancel the after event running in the window
+
+                # Remove the tkinter objects from the window and set them to None so that we can reenable them should the timer be restarted
+                timer_label.destroy()
+                pause_button.destroy()
+                timer_label = None
+                pause_button = None
+
+            timed_event = win.after(1500, lambda: erase_label(timed_event)) # Create a win.after() event that erases the label after 1.5 seconds
+            win.after_cancel(timer) # Cancel the countdown timer
 
     update_timer(seconds) # Call the update_timer function with the amount of seconds passed to it
+
 
 # Function to get the image as a TK PhotoImage using PIL
 def open_image(filename):
@@ -82,7 +125,7 @@ video_label = tk.Label(win, image = thumbnail_img) # Create a label that display
 video_label.bind("<Button-1>", lambda _: webbrowser.open_new(video)) # Bind an event listener to the label that opens the video in the browser on left click
 video_label.pack() # Add the label to the window
 
-timer_button = tk.Button(win, text = "Start timer", command = lambda: start_timer(1800), bg = "#806350", fg = "#EDD9CC", font = ("Arial", 15, "bold")) # Create a button that calls the timer function when clicked with colors and fonts applied
+timer_button = tk.Button(win, text = "Start timer", command = lambda: start_timer(30), bg = "#806350", fg = "#EDD9CC", font = ("Arial", 15, "bold")) # Create a button that calls the timer function when clicked with colors and fonts applied
 timer_button.pack(padx = 10, pady = 10, ipadx = 10, ipady = 10) # Add the timer button to the window with internal and external padding
 
 win.bind("<Destroy>", delete_image_data) # Bind an event listener that invokes delete_image_data with _ as the event when the window is deleted
